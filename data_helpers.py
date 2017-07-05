f03b7430eeae0aa07c69f0e66729968773c03244
NightmareNyx <nightmare7777777@gmail.com>
2017-07-05 07:06:51 +0300

    text to graph dataset conversion - untested

--- data_helpers.py
+++ data_helpers.py
@@ -16,7 +16,9 @@ def load_data(ds_name, use_node_labels):
     node2graph = {}
     Gs = []
 
-    with open("./datasets/%s/%s_graph_indicator.txt" % (ds_name, ds_name), "r") as f:
+    ds_name_ = ds_name.split('/')[0]
+
+    with open("./datasets/%s/%s_graph_indicator.txt" % (ds_name, ds_name_), "r") as f:
         c = 1
         for line in f:
             node2graph[c] = int(line[:-1])
@@ -25,14 +27,14 @@ def load_data(ds_name, use_node_labels):
             Gs[-1].add_node(c)
             c += 1
 
-    with open("./datasets/%s/%s_A.txt" % (ds_name, ds_name), "r") as f:
+    with open("./datasets/%s/%s_A.txt" % (ds_name, ds_name_), "r") as f:
         for line in f:
             edge = line[:-1].split(",")
             edge[1] = edge[1].replace(" ", "")
             Gs[node2graph[int(edge[0])] - 1].add_edge(int(edge[0]), int(edge[1]))
 
     if use_node_labels:
-        with open("./datasets/%s/%s_node_labels.txt" % (ds_name, ds_name), "r") as f:
+        with open("./datasets/%s/%s_node_labels.txt" % (ds_name, ds_name_), "r") as f:
             c = 1
             for line in f:
                 node_label = int(line[:-1])
@@ -40,8 +42,10 @@ def load_data(ds_name, use_node_labels):
                 c += 1
 
     labels = []
-    with open("./datasets/%s/%s_graph_labels.txt" % (ds_name, ds_name), "r") as f:
+    with open("./datasets/%s/%s_graph_labels.txt" % (ds_name, ds_name_), "r") as f:
         for line in f:
+            if line is None or line[:-1] == '':
+                continue
             labels.append(int(line[:-1]))
 
     labels = np.array(labels, dtype=np.float)
--- graph_of_words.py
+++ graph_of_words.py
@@ -1,4 +1,17 @@
 import itertools
+import re
+from sklearn import preprocessing
+import os
+import numpy as np
+import pprintpp as pp
+
+
+def extract_terms(file_location, stopwords=None):
+    with open(file_location, 'r') as doc:
+        terms = re.compile('\w+').findall(doc.read().lower().replace('\n', ''))
+        if stopwords is not None:
+            terms = filter(lambda x: x not in stopwords, terms)
+        return terms
 
 
 def terms_to_graph(terms, w):  # terms=list w=window size
@@ -22,11 +35,11 @@ def terms_to_graph(terms, w):  # terms=list w=window size
         # term to consider
         considered_term = terms[i]
         # all terms within sliding window
-        terms_temp = terms[(i-w+1):(i+1)]
+        terms_temp = terms[(i - w + 1):(i + 1)]
 
         # edges to try
         candidate_edges = list()
-        for p in range(w-1):
+        for p in range(w - 1):
             candidate_edges.append((terms_temp[p], considered_term))
 
         for try_edge in candidate_edges:
@@ -47,3 +60,100 @@ def terms_to_graph(terms, w):  # terms=list w=window size
                 # if edge has never been seen, create it and assign it a unit weight
                 else:
                     from_to[try_edge] = 1
+
+    return from_to
+
+
+def produce_graph_files(dataset, cats):
+    def get_text_to_write(arr):
+        to_write = ""
+        for i in range(len(arr)):
+            if i % 2 == 0:
+                to_write += str(arr[i]) + ', '
+            else:
+                to_write += str(arr[i]) + '\n'
+        return to_write
+
+    # data to be written in files
+    A_ = []
+    terms_ = []
+    graph_indicator_ = []
+    graph_labels_ = []
+    graph_labels_str_ = []
+    edge_labels_ = []
+
+    ds = './datasets/%s/' % dataset
+
+    # preprocessing
+    gid = 1  # graph indicator
+    for cat in cats.keys():
+        for doc in os.listdir(ds + cat):
+            terms = extract_terms(ds + cat + '/' + doc)
+            graph = terms_to_graph(terms, 2)
+
+            termlist = [[*x] for x in graph.keys()]
+            termlist = np.array(termlist).flatten().tolist()
+
+            # just the terms
+            terms_.extend(termlist)
+            # append the graph indicator to each term to differentiate the same terms of different documents and classes
+            # this is useful later when label encoding is performed
+            A_.extend([t + doc for t in termlist])
+            # each edge has a weight
+            edge_labels_.extend([w for w in graph.values()])
+            # gid for each node in the graph - unique term
+            graph_indicator_.extend([gid for _ in range(len(set(terms)))])
+            # label for each graph as integer
+            graph_labels_.append(cats[cat])
+            # label for each graph as string
+            graph_labels_str_.append(cat)
+
+            gid += 1
+
+    le = preprocessing.LabelEncoder()
+    A_ = le.fit_transform(A_) + 1
+    A_ = A_.tolist()
+    A_ = get_text_to_write(A_)
+    terms_ = get_text_to_write(terms_)
+    edge_labels_ = '\n'.join([str(x) for x in edge_labels_])
+    graph_indicator_ = '\n'.join([str(x) for x in graph_indicator_])
+    graph_labels_ = '\n'.join([str(x) for x in graph_labels_])
+    graph_labels_str_ = '\n'.join(graph_labels_str_)
+
+    # files
+    gds = ds + 'graph/'
+    os.makedirs(os.path.dirname(gds), exist_ok=True)
+    with open(gds + dataset + '_terms.txt', 'w') as terms_file:
+        terms_file.write(terms_)
+    with open(gds + dataset + '_A.txt', 'w') as A_file:
+        A_file.write(A_)
+    with open(gds + dataset + '_edge_labels.txt', 'w') as edge_labels_file:
+        edge_labels_file.write(edge_labels_)
+    with open(gds + dataset + '_graph_indicator.txt', 'w') as graph_indicator_file:
+        graph_indicator_file.write(graph_indicator_)
+    with open(gds + dataset + '_graph_labels.txt', 'w') as graph_labels_file:
+        graph_labels_file.write(graph_labels_)
+    with open(gds + dataset + '_graph_labels_str.txt', 'w') as graph_labels_str_file:
+        graph_labels_str_file.write(graph_labels_str_)
+
+
+if __name__ == "__main__":
+    # encodings
+    cats = {
+        'business': 1,
+        'entertainment': 2,
+        'politics': 3,
+        'sport': 4,
+        'tech': 5
+    }
+
+    scats = {
+        'athletics': 1,
+        'cricket': 2,
+        'football': 3,
+        'rugby': 4,
+        'tennis': 5
+    }
+
+    produce_graph_files('bbcsport', scats)
+    produce_graph_files('bbc', cats)
